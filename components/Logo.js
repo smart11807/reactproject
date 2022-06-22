3f3f4a72304e0a936e7a81c536d04cbf00a7b0a5
smart11807 <labinstall@tnistudent.ac.th>
2022-06-22 09:22:08 +0700

    first commit

---
+++ components/Logo.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <View>
+      <Text style={styles.TextLogo}>TNI</Text>
+    </View>
+  )
+}
+
+export default Logo
+
+const styles = StyleSheet.create({
+    TextLogo:{
+        color:"yellow",
+        fontSize:40
+    }
+})
---
+++ components/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native-web";
+
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      justifyContent:'center',
+      alignContent:'center'
+    },
+    title:{
+      fontSize:25,
+    },
+    warning:{
+      color:'red'
+    }
+  });
+
+  export default styles
