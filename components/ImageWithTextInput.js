9127598a0f102e510427072d5d1c12f5cb7cc571
smart11807 <smart.207077@gmail.com>
2022-07-20 11:39:59 +0700

    new commit

--- components/ImageWithTextInput.js
+++ components/ImageWithTextInput.js
@@ -1,57 +1,62 @@
-import { StyleSheet, Text, View, TextInput, Image } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native'
+import React from 'react'
 
 const ImageWithTextInput = () => {
-  return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <View style={styles.sectionStyle}>
-          <Image
-            source={require("../assets/input_username.png")}
-            style={styles.imageStyle}
-          />
-          <TextInput style={{ flex: 1 }} placeholder="Enter Your Name Here" />
+    return (
+        <View style={{ flex: 1 }}>
+            <View style={styles.container}>
+            <View style={styles.sectionStyle}>
+                    <Image
+                        source={require('../assets/input_username.png')}
+                        style={styles.imageStyle}
+                    />
+                    <TextInput
+                        style={{ flex: 1 }}
+                        placeholder='Enter Your Name Here'
+                    />
+                </View>
+                <View style={styles.sectionStyle}>
+                    <Image
+                        source={require('../assets/input_phone.png')}
+                        style={styles.imageStyle}
+                    />
+                    <TextInput
+                        style={{ flex: 1 }}
+                        placeholder='Enter Your Phone Here'
+                    />
+                </View>
+            </View>
         </View>
-        <Image
-          source={require("../assets/input_phone.png")}
-          style={styles.imageStyle}
-        />
-        <TextInput
-          style={{ flex: 1 }}
-          placeholder="Enter Your Phone-Number Here"
-        />
-      </View>
-    </View>
-  );
-};
+    )
+}
 
-export default ImageWithTextInput;
+export default ImageWithTextInput
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    margin: 10,
-  },
-  sectionStyle: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-    borderWidth: 0.5,
-    borderColor: "#000",
-    height: 40,
-    borderRadius: 5,
-    margin: 10,
-  },
-
-  imageStyle: {
-    padding: 10,
-    margin: 5,
-    height: 25,
-    width: 25,
-    resizeMode: "stretch",
-    alignItems: "center",
-  },
-});
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10,
+    },
+    sectionStyle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        borderWidth: 0.5,
+        borderColor: '#000',
+        height: 40,
+        width : '80%',
+        borderRadius: 5,
+        margin: 10,
+    },
+    imageStyle: {
+        padding: 10,
+        margin: 5,
+        height: 25,
+        width: 25,
+        resizeMode: 'stretch',
+        alignItems: 'center',
+    }
+})
