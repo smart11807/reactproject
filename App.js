317df2036474b79c414b6d84e2aada6502c23cd3
DISAWAT​ SRISAMRAN <58892343+smart11807@users.noreply.github.com>
2022-07-30 02:40:13 +0700

    new commit

--- App.js
+++ App.js
@@ -11,24 +11,14 @@ import AlertComponents from "./components/AlertComponents";
 import ImageWithTextInput from "./components/ImageWithTextInput";
 import ButtonExample from "./components/ButtonExample";
 import Touchable_Example from "./components/Touchable_Example";
-import TouchablePractice from "./TouchablePractice";
+import TouchablePractice from "./components/TouchablePractice";
+import DynamicStyle from './components/DynamicStyle'
+import ModalExample from './components/ModalExample'
+import ModalPractice from './components/ModalPractice'
 const App = () => {
   return (
     <view>
-      {/* <Logo /> */}
-      {/* <User /> */}
-      {/* <LotsOfGreetings /> */}
-      {/* <MyCustomTextWith /> */}
-      {/* <Count /> */}
-      {/* <InputText /> */}
-      {/* <UserNameandPassword/>   */}
-      {/* <UserNamePassword /> */}
-      {/* <AlertComponents /> */}
-      {/* <ImageWithTextInput/> */}
-      {/* <ButtonExample/> */}
-      {/* <Touchable_Example/> */}
-      {/* <DynamicStyle/> */}
-      <ModelExample/>
+     <ModalPractice/>
     </view>
   );
 };
